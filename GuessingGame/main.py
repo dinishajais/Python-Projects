e86eb51de31485ae0f7e27225ce1de925b13b326
dinishajais <dinishajais3010@gmail.com>
2026-07-31 05:24:35 +0530

    functions learn

--- GuessingGame/main.py
+++ GuessingGame/main.py
@@ -1,27 +1,47 @@
 import random
 
-secret_number=random.randint(1,100)
-attempts=0
+def get_user_guess():
+    try:
+        return int(input("Enter a number: "))
+    except ValueError:
+        print("<Enter a valid number >")
+        return None
 
-try:
+def check_guess(secret_number,user_guess):
+    if user_guess==secret_number:
+        return "CORRECT"
+    elif user_guess<secret_number:
+        print("Too low !! Try again")
+        return "LOW"
+    else:
+        print("Too high !! Try again")
+        return "HIGH"
+
+        
+def play_game():
+    attempts=0
+    secret_number=random.randint(1,100)
+
     while True:
-        try:
-            user_guess=int(input("Enter a number: "))
-        except ValueError:
-            print("<Enter a valid number >")
-            attempts+=1
+
+        user_guess=get_user_guess()
+        if(user_guess is None):
             continue
         attempts+=1
-
-        if user_guess==secret_number:
+        result=check_guess(secret_number,user_guess)
+        if(result=="CORRECT"):
             print(f"Bingo !! Correct guess in {attempts} attempt")
             break
-        elif user_guess<secret_number:
-            print("Too low !! Try again")
-        else:
-            print("Too high !! Try again")
-except:
-    print("An exception has occured")
+
+def main():
+    play_game()
+
+main()
+
+        
+
+        
+
 
 
 
